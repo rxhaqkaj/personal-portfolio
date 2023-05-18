fd4f73289d0bde35d72eeacb12ba3c7a33131e71
rxhaqkaj <rilinda@centralsoft.io>
2023-05-18 15:17:20 +0200

    feat(about-page): about me section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import About from './components/about/About';
 
 const App = () => {
   return (
     <>
       <Header />
       <main className='main'>
-        <Home />
+        <Home />  
+        <About />
       </main>
     </>
   );
