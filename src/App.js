16bf7debce2f5bb2b14f46e7a6d32e378d763564
rxhaqkaj <rilinda@centralsoft.io>
2023-05-24 10:39:59 +0200

    feat(skills-page): skills section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Skills from './components/skills/Skills';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='main'>
+        <Home />  
+        <About />
+        <Skills/>
+      </main>
+    </>
+  );
+}
+
+export default App;
+import './App.css';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import About from './components/about/About';
 
 const App = () => {
   return (
