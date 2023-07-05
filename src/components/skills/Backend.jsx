8af09a7938a4d0179e9952f2c76d9ee03a87b6ff
rxhaqkaj <rilinda@centralsoft.io>
2023-07-05 16:06:16 +0200

    fix: correct skills name

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -9,7 +9,7 @@ const Backend = () => {
                     <div className="skills__data">
                         <i class="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name">Node Js</h3>
+                            <h3 className="skills__name">Node JS</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
@@ -32,8 +32,8 @@ const Backend = () => {
                 <div className="skills__data">
                         <i class="bx bx-badge-check"></i>
                         <div>
-                            <h3 className="skills__name">PHP</h3>
-                            <span className="skills__level">Basic</span>
+                            <h3 className="skills__name">Express JS</h3>
+                            <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
                     <div className="skills__data">
